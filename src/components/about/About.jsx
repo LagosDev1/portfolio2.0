fb0c3fff89eea96ddeb01dc8e22a88c2eb214ce2
abahanointed@gmail.com <abahanointed@gmail.com>
2023-11-13 23:10:04 +0100

    new annimations

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -16,8 +16,8 @@ const About = () => {
   <section id= 'about'> 
     <h5>Get to know</h5>
     <h2>About Me</h2> 
-    <div className='container about__container' data-aos = "fade-right" data-aos-easing = "ease-out-cubic" data-aos-duration = "10000" >
-          <div className="about__me">
+    <div className='container about__container' >
+          <div className="about__me" data-aos = "fade-right" data-aos-easing = "ease-out-cubic" data-aos-duration = "10000">
             <div className="about__meAbout">
               <img src={meAbout} alt="About Image" />
             </div>
@@ -25,26 +25,26 @@ const About = () => {
           
           <div className="about__content">
               <div className="about__cards">
-                <article className='about__card'>
-                    <FaAward className='about__icon'/>
+                <article className='about__card' data-aos = "fade-up" data-aos-easing = "ease-out-cubic" data-aos-duration = "10000">
+                    <FaAward className='about__icon'data-aos = "zoom-up" data-aos-easing = "ease-out-cubic" data-aos-duration = "10000"/>
                     <h5>Experience</h5>
                     <small>3+ Years Working Experience</small>
                 </article>
 
-                <article className='about__card'>
+                <article className='about__card'data-aos = "fade-up" data-aos-easing = "ease-out-cubic" data-aos-duration = "10000">
                     <FiUsers className='about__icon'/>
                     <h5>Clients</h5>
                     <small>200+ Worldwide</small>
                 </article>
 
-                <article className='about__card'>
+                <article className='about__card'data-aos = "fade-up" data-aos-easing = "ease-out-cubic" data-aos-duration = "10000">
                     <AiFillFolderOpen className='about__icon'/>
                     <h5>Projects</h5>
                     <small>80+ completed projects</small>
                 </article>
               </div>
 
-              <p>
+              <p data-aos = "fade-right" data-aos-easing = "ease-out-cubic" data-aos-duration = "10000">
               I am a software developer, I design, develop, 
               test, and maintain software applications using languages such as JavaScript,
                Python, and PHP, along with frameworks like ReactJs, and tools like Git, GitHub,
@@ -52,7 +52,7 @@ const About = () => {
                 creativity, good communication skills, and thrive in collaborative environments.
               </p>
               
-              <a href="#contact" className='btn btn-primary'> Let's Talk</a>
+              <a href="#contact" className='btn btn-primary' data-aos = "fade-up" data-aos-easing = "ease-out-cubic" data-aos-duration = "10000"> Let's Talk</a>
           </div>
 
     </div>
