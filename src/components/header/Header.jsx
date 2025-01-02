bdbb9c78b7c489f974fdb23b3522b6b22b176568
GODSPOWER🥶 <abahanointed@gmail.com>
2025-01-02 03:25:55 +0100

    data-aos for header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,15 +12,13 @@ const Header = () => {
   }, [])
   return (
     <header>
-      <div className="container header__container" data-aos = "fade-left"
-            data-aos-easing = "ease-out-cubic"
-            data-aos-duration = "10000">
+      <div className="container header__container" data-aos = "fade-up" data-aos-easing = "ease-out-cubic" data-aos-duration = "50000">
         <h5>Hello I'm</h5>
         <h1>Godspower Abah</h1>
         <h5 className="fullstack">Fullstack Developer</h5>
         <CTA />
         <HeaderSocials/>
-        <div className="mainIMAGE" data-aos = "fade-left">
+        <div className="mainIMAGE" data-aos = "fade-up" data-aos-easing = "ease-out-cubic" data-aos-duration = "50000">
         <img src={mainIMAGE} alt="" height= '100%'/>
         </div>
         <a href="#contact" className='scroll__down'>Scroll Down</a>
