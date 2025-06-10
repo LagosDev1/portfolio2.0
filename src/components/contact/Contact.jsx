32106235d1994ba8475433dc621b761daa68db16
lagosDev1 <abahanointed@gmail.com>
2025-06-10 13:03:02 +0100

    latest change

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -24,7 +24,7 @@ const Contact = () => {
       e.target.reset();
    };
 
-   
+  // Reset the messageSent state after a certain time
   return (
     <section id='contact'>
       <h5 data-aos-easing = "ease-out-cubic" data-aos-duration = "50000">Get In Touch</h5>
