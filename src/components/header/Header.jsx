e79990866cc19282b90cf6b08468ea12449ceb6a
LagosDev1 <abahanointed@gmail.com>
2026-01-06 22:55:55 +0100

    Latest

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
       <div className="container header__container" data-aos = "fade-up" data-aos-easing = "ease-out-cubic" data-aos-duration = "50000">
         <h5>Hello, I am</h5>
         <h1>Godspower Abah </h1>
-        <h5 className="fullstack">Fullstack Developer</h5>
+        <h5 className="fullstack">Software Developer</h5>
         <CTA />
         <HeaderSocials/>
         <div className="mainIMAGE" data-aos = "fade-up" data-aos-easing = "ease-out-cubic" data-aos-duration = "50000">
