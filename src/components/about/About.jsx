08601503af104f0579713308a5c8743cacccc04e
LagosDev1 <abahanointed@gmail.com>
2026-02-18 01:27:04 +0100

    new changes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -13,6 +13,7 @@ const About = () => {
     Aos.init();
   }, [])
 
+1
   return (
   <section id= 'about'> 
     <h5 data-aos = "fade-up" data-aos-easing = "ease-out-cubic" data-aos-duration = "50000">Get to know</h5>
